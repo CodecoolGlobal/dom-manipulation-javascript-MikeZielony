b2667460874358f6bb8a1c782da845445624f274
MikeZielony <51535787+MikeZielony@users.noreply.github.com>
2020-05-07 07:05:43 +0200

    Refactor code

--- task-1/form_rows.js
+++ task-1/form_rows.js
@@ -1,13 +1,31 @@
-function removeRow() {
-    let row = document.getElementsByClassName("row");
-    row.remove();
+var counter = 1;
+
+function addInput(divName, template) {
+    if (counter == 10) {
+        alert("Too much elements :  " + counter);
+        document.getElementById("add_more_text").remove();
+    } else {
+        counter++;
+        alert("Numbers of elements :  " + counter);
+        var newdiv = document.createElement('div');
+        newdiv.innerHTML = document.getElementById(divName).innerHTML;
+        newdiv.className = 'added';
+        document.getElementById(template).appendChild(newdiv);
+        
+    }
+
+    var selectElements = document.querySelectorAll('select');
+    for (var i = 0; i < selectElements.length; i++) {
+        selectElements[i].id = 'id-' + i;
+        selectElements[i].name = 'category' + i;
+    }
 }
 
-
-var original = document.getElementById('duplicater');
-
-function duplicate() {
-    var clone = original.cloneNode(true); // 
-    clone.id = "duplicater";
-    original.parentNode.appendChild(clone);
+function removeInput(obj) {
+    counter--;
+    if (obj.parentNode.className == 'added') {
+        alert("Numbers of elements :  " + counter);
+        obj.parentNode.parentNode.removeChild(obj.parentNode);
+        
+    }
 }
